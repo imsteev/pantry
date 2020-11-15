fbf556514be115bbbe59541bce8673eba5a903a2
Stephen Chung <spchung95@gmail.com>
2020-11-15 11:27:18 -0800

    deno fmt

--- cache.test.ts
+++ cache.test.ts
@@ -1,9 +1,9 @@
-import PantryCache from './cache';
+import PantryCache from "./cache";
 
-describe('cache tests', () => {
-  it('smoke test', () => {
+describe("cache tests", () => {
+  it("smoke test", () => {
     const pantry = new PantryCache();
-    pantry.put('hello', 'world');
-    expect(pantry.get('hello')).toEqual('world');
-  })
-})
+    pantry.put("hello", "world");
+    expect(pantry.get("hello")).toEqual("world");
+  });
+});
